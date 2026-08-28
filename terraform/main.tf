cdec7c1dce06b0ba5d1d21c681947454fdca8aa5
ayo <ddraven009@gmail.com>
2026-08-28 12:48:27 +0100

    Add lambda and API gateway for visitor counter

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,16 +52,16 @@ resource "aws_cloudfront_distribution" "cdn" {
   default_root_object = "index.html"
   aliases             = ["ayomideobadina.com", "www.ayomideobadina.com"]
   origin {
-    domain_name = aws_s3_bucket.portfolio.bucket_regional_domain_name
-    origin_id   = "s3-origin"
+    domain_name              = aws_s3_bucket.portfolio.bucket_regional_domain_name
+    origin_id                = "s3-origin"
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
   default_cache_behavior {
     target_origin_id       = "s3-origin"
     viewer_protocol_policy = "redirect-to-https"
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods  = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = false
@@ -71,7 +71,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
   }
 
-viewer_certificate {
+  viewer_certificate {
     acm_certificate_arn      = data.aws_acm_certificate.portfolio.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
